87d94f6186564750e4fc9b96a11c8bdca86750c2
dungna <dungna2401@gmail.com>
2017-07-25 12:02:08 +0700

    add pakage json

--- getAge.js
+++ getAge.js
@@ -1,17 +1,16 @@
-class person {
-  getAge (age) {
-    let countDownDate = new Date(age)
-    countDownDate = countDownDate.getFullYear()
+class Person {
+    getAge(age) {
+        let countDownDate = new Date(age)
+        countDownDate = countDownDate.getFullYear()
 
-    if (Number.isNaN(countDownDate)) {
-	  return 'fail'
-    } else {
-      // Get todays date and time
-      let now = new Date().getFullYear()
-      let year = Math.floor(now - countDownDate)
-      console.log(countDownDate)
-      return year
+        if (Number.isNaN(countDownDate)) {
+            return false
+        } else {
+            // Get todays date and time
+            let now = new Date().getFullYear()
+            let year = Math.floor(now - countDownDate)
+            return year
+        }
     }
-  }
 }
-module.exports = new person()
+module.exports = new Person()
--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 'use strict';
- 
-var express = require('express');
-var app = express();
+
+const express = require('express');
+const app = express();
+const person = require('./getAge')
 const bodyParser = require('body-parser')
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({extended: false}))
 // parse application/json
 app.use(bodyParser.json())
-var server = app.listen('3000', function () {
-    var host = server.address().address;
-    var port = server.address().port;
+const server = app.listen('3000', function () {
+    let host = server.address().address;
+    let port = server.address().port;
     console.log('Server start at http://%s:%s', '0.0.0.0', 3000);
 });
-
-app.get('/hello', function(req, res){
-    res.send(JSON.stringify({'hello': 'world !!!'}));
-});
-var person = require('./getAge')
 app.post('/calculate-age', function (req, res) {
     let birthday = req.body.birthday
-    console.log(birthday)
-   // res.send({success: true, age: JSON.stringify(21)})
     if (birthday == 'fail') {
         res.send({success: false, message: 'format birthday yyyy/m/d'})
     } else {
@@ -33,16 +27,16 @@ app.post('/calculate-age', function (req, res) {
     }
 })
 process.argv.slice(2).map(function (y, i) {
-  let birthDay = y.split('=')
-  if (birthDay[0] === '--birthday') {
-    let age = person.getAge(
-	[1])
-    if (age == 'fail') {
-      console.log('format birthday yyyy/m/d')
+    let birthDay = y.split('=')
+    if (birthDay[0] === '--birthday') {
+        let age = person.getAge(
+            [1])
+        if (age == 'fail') {
+            console.log('format birthday yyyy/m/d')
+        } else {
+            console.log({age: JSON.stringify(age)})
+        }
     } else {
-      console.log({age: JSON.stringify(age)})
+        console.log('key is birthday')
     }
-  } else {
-    console.log('key is birthday')
-  }
 })
