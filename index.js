c06ec745c24c5af7438a18507b3fdade35b450b4
dungna <dungna2401@gmail.com>
2017-07-25 09:07:29 +0700

    update post /canculate-age

--- index.js
+++ index.js
@@ -2,35 +2,35 @@
  
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 var server = app.listen('3000', function () {
     var host = server.address().address;
     var port = server.address().port;
     console.log('Server start at http://%s:%s', '0.0.0.0', 3000);
 });
+
 app.get('/hello', function(req, res){
     res.send(JSON.stringify({'hello': 'world !!!'}));
 });
-const bodyParser = require('body-parser')
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
 var person = require('./getAge')
 app.post('/calculate-age', function (req, res) {
-  let birthday = req.body.birthday
-    console.log(req.body)
-  if (birthday == 'fail') {
-    res.send({success: false, message: 'format birthday yyyy/m/d'})
-  } else {
-    if (birthday.length == 0) {
-     res.send({success: false, message: 'birthday is empty'})
+    let birthday = req.body.birthday
+    console.log(birthday)
+   // res.send({success: true, age: JSON.stringify(21)})
+    if (birthday == 'fail') {
+        res.send({success: false, message: 'format birthday yyyy/m/d'})
     } else {
-     let data = person.getAge(birthday)
-     res.send({success: true, age: JSON.stringify(data)})
-    } 
-  }
-  
+        if (birthday.length == 0) {
+            res.send({success: false, message: 'birthday is empty'})
+        } else {
+            let data = person.getAge(birthday)
+            res.send({success: true, age: JSON.stringify(data)})
+        }
+    }
 })
 process.argv.slice(2).map(function (y, i) {
   let birthDay = y.split('=')
